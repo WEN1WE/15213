d1e81bc5742d2a7df13eb1d40312b82748217012
WEN1WE <wwen8973@gmail.com>
2018-04-08 00:56:07 +0800

    cachelab

---
+++ cachelab/csim.c
@@ -0,0 +1,147 @@
+#include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <unistd.h>
+
+void printHelpInfo();
+void readInput();
+void readFile();
+void initCache();
+void accessCache();
+
+
+unsigned s, E, b;
+unsigned hit_count = 0, miss_count = 0, eviction_count = 0;
+FILE *pFile;
+
+char operation;
+int address;
+int size;
+
+typedef struct {
+	short Valid_bit;
+	int Tag;
+	int LRU_counter;
+} cache_line;
+cache_line **cache;
+
+int main(int argc, char **argv) {
+	readInput(argc, argv);
+	initCache();
+	readFile();
+
+	printSummary(hit_count, miss_count, eviction_count);
+
+	return 0;
+}
+
+void readFile() {
+	while (fscanf(pFile, " %c %x,%d", &operation, &address, &size) > 0) {
+		switch (operation) {
+		case 'L':
+			accessCache();
+			break;
+		case 'M':
+			accessCache();
+		case 'S':
+			accessCache();
+			break;
+		}
+	}
+}
+
+
+void readInput(int argc, char **argv) {
+	int opt;
+	while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+		switch (opt) {
+		case 's':
+			s = atoi(optarg);
+			break;
+		case 'E':
+			E = atoi(optarg);
+			break;
+		case 'b':
+			b = atoi(optarg);
+			break;
+		case 't':
+			pFile = fopen(optarg, "r");
+			break;
+		case 'h':
+		default:
+			printHelpInfo();
+			break;
+		}
+	}
+}
+
+
+void initCache() {
+	unsigned S = (1 << s);
+	cache = (cache_line **)malloc(sizeof(cache_line*) << s);
+	for (int i = 0; i < S; i++) {
+		cache[i] = (cache_line *)malloc(sizeof(cache_line) * E);
+	}
+
+	for (int i = 0; i < S; i++) {
+		for (int j = 0; j < E; j++) {
+			cache[i][j].LRU_counter = -1;
+			cache[i][j].Valid_bit = 0;
+			cache[i][j].Tag = -1;
+		}
+	}
+}
+
+void accessCache() {
+	int tag = address >> b >> s;
+	int setIndex = (address >> b) ^ (tag << s);
+	int evictionIndex = 0;
+
+
+	for (int i = 0; i < E; i++) {
+		if (cache[setIndex][i].LRU_counter > cache[setIndex][evictionIndex].LRU_counter) {
+			evictionIndex = i;
+		}
+
+		if (cache[setIndex][i].Valid_bit == 1) {
+			if (cache[setIndex][i].Tag == tag) {
+				cache[setIndex][i].LRU_counter = 0;
+				hit_count++;
+				return;
+			}
+			else {
+				cache[setIndex][i].LRU_counter++;
+				continue;
+			}
+		}
+		else {
+			miss_count++;
+			cache[setIndex][i].Tag = tag;
+			cache[setIndex][i].LRU_counter = 0;
+			cache[setIndex][i].Valid_bit = 1;
+			return;
+		}
+	}
+
+	eviction_count++;
+	miss_count++;
+
+	cache[setIndex][evictionIndex].Tag = tag;
+	cache[setIndex][evictionIndex].LRU_counter = 0;
+}
+
+void printHelpInfo() {
+	printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+	printf("Options:\n");
+	printf("  -h         Print this help message.\n");
+	printf("  -v         Optional verbose flag.\n");
+	printf("  -s <num>   Number of set index bits.\n");
+	printf("  -E <num>   Number of lines per set.\n");
+	printf("  -b <num>   Number of block offset bits.\n");
+	printf("  -t <file>  Trace file.\n\n");
+	printf("Examples:\n");
+	printf("  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+	printf("  linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+}
+
